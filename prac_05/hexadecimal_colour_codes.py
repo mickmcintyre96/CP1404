2e95ef069463d5bc921bd7520293762f50d1281b
mickmcintyre96 <30308246+mickmcintyre96@users.noreply.github.com>
2017-08-31 10:04:45 +1000

    prac 5 updated

--- prac_05/hexadecimal_colour_codes.py
+++ prac_05/hexadecimal_colour_codes.py
@@ -1,5 +1,7 @@
-HEXADEXIMAL_COLOUR_CODES = {"AliceBlue": "#f0f8ff" , "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc","AntiqueWhite3":
-    "#cdc0b0","AntiqueWhite4": "#8b8378","aquamarine1": "#7fffd4","aquamarine2": "#76eec6","aquamarine4": "#458b74","azure1": "#f0ffff}"}
+HEXADEXIMAL_COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
+                            "AntiqueWhite2": "#eedfcc", "AntiqueWhite3":
+                                "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4",
+                            "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff}"}
 
 colour = input("Enter colour name: ")
 while colour != "":
